9b4c5c4710a171e8e7ab637c5f8758afbf139f13
user <user@NOVIA-PC>
2021-08-07 21:38:14 +0800

    加入專案檔案。

---
+++ yc_interview/Models/DB/ProductsAboveAveragePrice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace yc_interview.Models.DB
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
